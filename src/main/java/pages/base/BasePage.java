2c9f8f74062eaf3651f671d63b596a9aa5a7bed2
darina.Ilenko <dilenko13@gmail.com>
2021-12-31 15:10:50 +0200

    updated BasePage

--- src/main/java/pages/base/BasePage.java
+++ src/main/java/pages/base/BasePage.java
@@ -1,46 +1,42 @@
-//package pages.base;
-//
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//
-//import static constants.Constant.TimeoutVariables.EXPLICIT_WAIT;
-//
-//public class BasePage {
-//    public WebDriver driver;
-//    public BasePage(WebDriver driver) {
-//        this.driver = driver;
-//    }
-//
-//    public final By authWidget = By.xpath("//iframe[@src='https://login-widget.privat24.ua/']");
-//
-//    /**
-//     *
-//     * The method for navigating to a specific URL
-//     */
-//    public void gotoUrl(String url){
-//        driver.get(url);
-//    }
-//
-//    /**
-//     *
-//     * Wait for visibility element in DOM model
-//     */
-//
-//    public WebElement waitElementIsVisible(WebElement element){
-//        new WebDriverWait(driver, EXPLICIT_WAIT).until(ExpectedConditions.visibilityOf(element));
-//        return element;
-//    }
-//
-//
-//    /**
-//     * Check is auth frame is
-//     */
-//    public void isAuthWidgetPresent(){
-//        WebElement authFrame = driver.findElement(authWidget);
-//        waitElementIsVisible(authFrame);
-//
-//    }
-//}
+package pages.base;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
+import static constants.Constant.TimeoutVariables.EXPLICIT_WAIT;
+
+public class BasePage {
+
+    public final SelenideElement authWidgetP24New = $x("//iframe[@src='https://login-widget.privat24.ua/']");
+
+    /**
+     *
+     * The method for navigating to a specific URL
+     */
+    public void gotoUrl(String url){
+        open(url);
+    }
+
+protected void clearAndType(SelenideElement element, String value){
+        while( !element.getAttribute("value").equals("")) element.sendKeys(Keys.BACK_SPACE);
+        element.sendKeys(value);
+}
+
+    /**
+     * Check is auth frame is
+     */
+    public void checkIsDisplayedAuthWidget(){
+    authWidgetP24New.shouldBe(Condition.visible);
+
+    }
+}
